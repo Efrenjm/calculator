a841c83d57890b6c093dc0e37493e640020653c1
Efren Jimenez <efrenjimenez_96@hotmail.com>
2023-03-17 05:03:15 -0600

    animation for all de the buttons

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 let buttons = document.getElementsByClassName("button");
 let current = document.getElementById("current")
 
+const dark = "rgb(44, 44, 44)";
+const white = "rgb(255, 255, 255)";
+const gray = "rgb(175, 178, 184)";
+const orange = "rgb(255, 165, 0)";
+
+function animationParameters(bgColor,fontColor){
+    return [
+            { backgroundColor: bgColor, color: fontColor },
+            { backgroundColor: fontColor, color: bgColor },
+            { backgroundColor: bgColor, color: fontColor },
+            ];
+}
+
+const numberButtonsAnimation = animationParameters(dark,white);
+const specialButtonsAnimation = animationParameters(gray,white);
+const eqButtonAnimation = animationParameters(orange,white);
+const timing = {
+    duration: 250,
+    iterations: 1,
+};
+
+
     // AC button 
 buttons[0].addEventListener("click",() => current.innerHTML = 0);
 
-    // Numbers buttons 
+    // Special buttons animation
+for (i=0; i<3; i++){
+    buttons[i].addEventListener("click", function(){
+        this.animate(specialButtonsAnimation, timing);
+    });
+}
+
+    // Number buttons 
 for (i of [4,5,6,8,9,10,12,13,14,16]){
     buttons[i].addEventListener("click", function(){
-        current.innerHTML = current.innerHTML == "0" ? this.innerHTML : current.innerHTML + this.innerHTML
-        this.style.animation = "example"
-        // console.log(this.parentNode.replaceChild(this.cloneNode(true),this))
-        this.classList.remove("button");
-        this.offsetWidth;
-        this.classList.add("button");
-        // this.style.backgroundColor = "white"
-        // this.style.color = "rgb(45,45,45)"
-        
-        // this.style.backgroundColor = "rgb(45,45,45)"
-        // this.style.color = "white"
+        current.innerHTML = current.innerHTML == "0" ? this.innerHTML : current.innerHTML + this.innerHTML;
+        this.animate(numberButtonsAnimation, timing);
     });
 }
 
     // Dot functionality 
-buttons[17].addEventListener("click", () => !current.innerHTML.includes(".") ? current.innerHTML = current.innerHTML + ".":null)
+buttons[17].addEventListener("click", function(){
+    !current.innerHTML.includes(".") ? current.innerHTML = current.innerHTML + "." : null;
+    this.animate(numberButtonsAnimation,timing)
+})
 
     // Operational buttons 
 for(i of [3,7,11,15]){
