db0f0ec30369f6fe192813c0e05c6fef27013d84
Efren Jimenez <efrenjimenez_96@hotmail.com>
2023-03-22 17:48:32 -0600

    Add calculation function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let buttons = document.getElementsByClassName("button");
-let current = document.getElementById("current")
+let current = document.getElementById("current");
 
     // Colors used
 const dark = "rgb(44, 44, 44)";
@@ -30,41 +30,74 @@ const operationalButtons = {
     "×":7,
     "+":11,
     "-":15,
-}
+};
     
-let operationMarked = null;  // Math operator selected
+let operationMarked = null;  // Operator selected
+let firstNumber = true; // After an operator
 let cachedNumber = null;  // Store the number in the display
+let cachedOperator = null;
 let negative = false;  // Negative number
-
-    // Special buttons animation
-// for (i=0; i<3; i++){
-//     buttons[i].addEventListener("click", function(){
-//         this.animate(specialButtonsAnimation, timing);
-//     });
-// }
+let limitNotReached = ()=> current.innerHTML.replaceAll(",","").replaceAll(".","").replaceAll("-","").length < 9;
+let operationSelected = ()=>operationalButtons[operationMarked];
+let unmarkOperationalButton = ()=>{
+    ind = operationSelected();
+    buttons[ind].style.backgroundColor = orange;
+    buttons[ind].style.color = white;
+    operationMarked = null;
+};
+let insertCommas = ()=>{
+    current.innerHTML = current.innerHTML.replaceAll("," , "");
+    intlen = current.innerHTML.includes(".") ? current.innerHTML.indexOf(".") : current.innerHTML.length;
+    commas = Math.floor((intlen-1-negative)/3);
+    for(i=0; i<commas; i++){
+        current.innerHTML = current.innerHTML.slice(0,intlen-3*(i+1)) + "," + current.innerHTML.slice(intlen-3*(i+1));
+    }
+}
+let calculate = ()=>{
+    switch (cachedOperator['operation']){
+        case '÷':
+            if (!cachedOperator['value']==0){
+                cachedNumber = cachedNumber/cachedOperator['value'];
+            }else{
+                cachedNumber = 'Error';
+                firstNumber = true;
+            }
+            break;
+        case '×':
+            cachedNumber = cachedNumber*cachedOperator['value'];
+            break;
+        case '+':
+            cachedNumber = cachedNumber+cachedOperator['value'];
+            break;
+        case '-':
+            cachedNumber = cachedNumber-cachedOperator['value'];
+            break;
+    }
+    current.innerHTML = cachedNumber;
+    cachedNumber !== 'Error' ? insertCommas() : null;
+    console.log(cachedNumber)
+}
 
     // Number buttons functionality 
 for (i of [4,5,6,8,9,10,12,13,14,16]){
     buttons[i].addEventListener("click", function(){
-        // if (negative){
-        //     current.innerHTML = current.innerHTML == "-0" ? "-"+this.innerHTML : current.innerHTML + this.innerHTML;
-        // }else{
-        //     current.innerHTML = current.innerHTML == "0" ? this.innerHTML : current.innerHTML + this.innerHTML;
-        //     // console.log(current.innerHTML.length)
-        // }
-
-
-            // Add numbers in the display
-        zero = negative ? "-0":"0";
-        current.innerHTML = current.innerHTML == zero ? (negative?"-":"")+this.innerHTML : current.innerHTML + this.innerHTML;
-            
-            // Insert commas at every 3 integer numbers
-        current.innerHTML = current.innerHTML.replaceAll("," , "");
-        intlen = current.innerHTML.includes(".")? current.innerHTML.indexOf(".") : current.innerHTML.length;
-        commas = Math.floor((intlen-1-negative)/3);
-        for(i=0; i<commas; i++){
-            current.innerHTML = current.innerHTML.slice(0,intlen-3*(i+1)) + "," + current.innerHTML.slice(intlen-3*(i+1)) 
+            // Insert numbers in the display
+        if(firstNumber){
+            current.innerHTML = 0;
+            firstNumber = false;
+            current.innerHTML = (negative?"-":"") + this.innerHTML;
+        }else{
+            limitNotReached() ? current.innerHTML += this.innerHTML : null;
         }
+
+            // Insert commas at every 3 integer numbers
+        insertCommas();
+        // current.innerHTML = current.innerHTML.replaceAll("," , "");
+        // intlen = current.innerHTML.includes(".") ? current.innerHTML.indexOf(".") : current.innerHTML.length;
+        // commas = Math.floor((intlen-1-negative)/3);
+        // for(i=0; i<commas; i++){
+        //     current.innerHTML = current.innerHTML.slice(0,intlen-3*(i+1)) + "," + current.innerHTML.slice(intlen-3*(i+1));
+        // }
 
             // Animation of the buttons
         this.animate(numberButtonsAnimation, timing);
@@ -74,9 +107,15 @@ for (i of [4,5,6,8,9,10,12,13,14,16]){
 
     // Dot button functionality 
 buttons[17].addEventListener("click", function(){
+        // Set display to zero after an operation
+    if(firstNumber){
+        current.innerHTML = (negative?"-0":"0");
+        firstNumber = false;
+    }
         // Addition to the floating point
-    current.innerHTML.includes(".") ? null : current.innerHTML = current.innerHTML + ".";
-        
+    if(limitNotReached()){
+        !current.innerHTML.includes(".") ? current.innerHTML += "." : null;
+    }
         // Animation of the button 
     this.animate(numberButtonsAnimation,timing);
     buttons[0].innerHTML = "C";
@@ -84,42 +123,38 @@ buttons[17].addEventListener("click", function(){
 
     // Plus/Minus button functionality
 buttons[1].addEventListener("click", function(){
+        // Set display to zero after an operation
+    firstNumber ? current.innerHTML = (negative?"-0":"0") : null;
         // Add or delete minus sign
     current.innerHTML = negative ? current.innerHTML.slice(1): "-" + current.innerHTML;
-    
-        // Invert bool value
-    negative = !negative
-
+        // Change the negative option
+    negative = !negative;
         // Animation of the button
     this.animate(specialButtonsAnimation, timing);
-
 })
 
-
-operationSelected = ()=>operationalButtons[operationMarked]
-function unmarkOperationalButton(ind){
-    buttons[ind].style.backgroundColor = orange;
-    buttons[ind].style.color = white;
-}
-
-    // Operational buttons 
+    // Math Operators buttons 
 for(i of [3,7,11,15]){
     buttons[i].addEventListener("click", function(){
             // Set current number in stage
-        cachedNumber = current.innerHTML
-
+        cachedNumber = parseFloat(current.innerHTML.replaceAll(",",""));
+        firstNumber = true;
+        negative = false;
         if(operationMarked){
-            ind = operationSelected()
+            ind = operationSelected();
                 // Animation when the clicked button was already selected
             if(ind == operationalButtons[this.innerHTML]){
-                this.animate(eqButtonAnimation.slice(0,2),timing)
+                this.animate(eqButtonAnimation.slice(0,2),timing);
             } else {
-                unmarkOperationalButton(ind);
+                unmarkOperationalButton();
             }
         }
             // Mark current clicked button and save it
         this.style.backgroundColor = white;
         this.style.color = orange;
+
+        // cachedOperator = {'operation': this.innerHTML, 'value': parseFloat(current.innerHTML.replaceAll(",",""))};
+
         operationMarked = this.innerHTML;
 
     });
@@ -128,23 +163,37 @@ for(i of [3,7,11,15]){
 
     // AC button 
 buttons[0].addEventListener("click",function() { 
-    if(operationMarked){
-            // Reset current display
-        // if ( this.innerHTML === "C" && current.innerHTML !== "0" ){ 
-        if (current.innerHTML !== "0" ){ 
-            current.innerHTML = 0;
-            this.innerHTML = "AC";
-        } else { 
-            ind = operationSelected();
-            unmarkOperationalButton(ind);
-            operationMarked = null;
-            parseInt(current.innerHTML)==0 ? buttons[0].innerHTML="AC" : null;
-        }
-    } else {
-        current.innerHTML = 0;
+
+    if(this.innerHTML=="AC"){
+            // Unmark math operational button
+        operationMarked ? unmarkOperationalButton() : null;
+        // cachedNumber = null;
+        cachedOperator = null;
+    }else{
         this.innerHTML = "AC";
+        firstNumber = true;
+        // cachedOperator ? cachedNumber = null : null;
     }
-    cachedNumber = null;
-    negative=false;
-    
+    cachedOperator ? cachedNumber = null : null;
+    current.innerHTML = 0;
+    negative = false;
+
+        // Animation
+    this.animate(specialButtonsAnimation, timing);
 });
+
+
+    // Equal button 
+buttons[18].addEventListener('click',function(){
+    if(operationMarked){
+        cachedOperator = {'operation': operationMarked, 'value': parseFloat(current.innerHTML.replaceAll(",",""))};
+        calculate();
+        unmarkOperationalButton();
+    }else{
+        cachedOperator ? calculate() : null;
+    }
+    console.log(cachedNumber)
+
+        // Animation
+    this.animate(eqButtonAnimation, timing);
+})
